237f72fda350b6b42d1144f54cd1da32242c2fa6
lisa850728g <lisa850728g@gmail.com>
2015-05-16 19:15:11 +0800

    update TestLoan class
    原本程式輸入的值若為0或負值時，會執行catch的部分，然後結束程式
    現在更改為，會一直讓使用者重複輸入數字，直到輸入的值大於0為止

--- TestLoan.java
+++ TestLoan.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 /**Loan class的測試程式
  * 讓使用者輸入loan amount . annual interest rate . number of years
- * 如果數值小於或等於0就會跑出IllegalArgumentException，結束程式
+ * 如果數值小於或等於0就會跑出IllegalArgumentException
+ * 重新讓使用者輸入新的值，直到輸入值大於0為止
+ * 才繼續執行下面的程式碼
  */
 public class TestLoan{
 	//Main method
@@ -13,53 +15,80 @@ public class TestLoan{
 		//create a Scanner
 		Scanner input = new Scanner(System.in);
 		
-		//測試是否有IllegalArgumentException被丟出
-		try{
-			//Prompt the user to enter the loan amount
-			System.out.println("Please enter the loan amount :");
-			double loanAmount = input.nextDouble();
+		//set a boolean initializes in true
+		boolean again = true;
 		
-			//呼叫Loan class的setLoanAmount method 並傳值進去
-			loan.setLoanAmount(loanAmount);
-		}
-		//抓出被丟出的IllegalArgumentException
-		catch(IllegalArgumentException exLoanAmount){
-			System.out.println(exLoanAmount);
-			//end programming
-			System.exit(1);
-		}
+		//重複執行直到傳入的值大於0
+		do{
+			//測試是否有IllegalArgumentException被丟出
+			try{
+				//Prompt the user to enter the loan amount
+				System.out.print("Please enter the loan amount : ");
+				double loanAmount = input.nextDouble();
 		
-		//測試是否有IllegalArgumentException被丟出
-		try{
-			//Prompt the user to enter the annual interest rate
-			System.out.println("Please enter the annual interest rate :");
-			double annualInterestRate = input.nextDouble();
+				//呼叫Loan class的setLoanAmount method 並傳值進去
+				loan.setLoanAmount(loanAmount);
+				
+				//reset again into false
+				again = false;
+			}
+			//抓出被丟出的IllegalArgumentException
+			catch(IllegalArgumentException exLoanAmount){
+				System.out.println(exLoanAmount);
+				//start from next line
+				System.out.println();
+			}
+		}while(again);
+		
+		//reset again into true
+		again = true;
+		
+		//重複執行直到傳入的值大於0
+		do{
+			//測試是否有IllegalArgumentException被丟出
+			try{
+				//Prompt the user to enter the annual interest rate
+				System.out.print("Please enter the annual interest rate : ");
+				double annualInterestRate = input.nextDouble();
 			
-			//呼叫Loan class的setAnnualInterestRate method 並傳值進去
-			loan.setAnnualInterestRate(annualInterestRate);
-		}
-		//抓出被丟出的IllegalArgumentException
-		catch(IllegalArgumentException exAnnualInterestRate){
-			System.out.println(exAnnualInterestRate);
-			//end programming
-			System.exit(1);
-		}
+				//呼叫Loan class的setAnnualInterestRate method 並傳值進去
+				loan.setAnnualInterestRate(annualInterestRate);
+				
+				//reset again into false
+				again = false;
+			}
+			//抓出被丟出的IllegalArgumentException
+			catch(IllegalArgumentException exAnnualInterestRate){
+				System.out.println(exAnnualInterestRate);
+				//start from next line
+				System.out.println();
+			}
+		}while(again);
 		
-		//測試是否有IllegalArgumentException被丟出
-		try{
-			//Prompt the user to enter the number of years
-			System.out.println("Please enter the number of years :");
-			int numberOfYears = input.nextInt();
+		//reset again into true
+		again = true;
 		
-			//呼叫Loan class的setNumberOfYears method 並傳值進去
-			loan.setNumberOfYears(numberOfYears);
-		}
-		//抓出被丟出的IllegalArgumentException
-		catch(IllegalArgumentException exNumberOfYears){
-			System.out.println(exNumberOfYears);
-			//end programming
-			System.exit(1);
-		}
+		//重複執行直到傳入的值大於0
+		do{
+			//測試是否有IllegalArgumentException被丟出
+			try{
+				//Prompt the user to enter the number of years
+				System.out.print("Please enter the number of years : ");
+				int numberOfYears = input.nextInt();
+		
+				//呼叫Loan class的setNumberOfYears method 並傳值進去
+				loan.setNumberOfYears(numberOfYears);
+				
+				//reset again into false
+				again = false;
+			}
+			//抓出被丟出的IllegalArgumentException
+			catch(IllegalArgumentException exNumberOfYears){
+				System.out.println(exNumberOfYears);
+				//start from next line
+				System.out.println();
+			}
+		}while(again);
 		
 		/*顯示出算完的monthly payment . total payment以及date*/
 		System.out.println("The monthly payment is : " + loan.getMonthlyPayment());
